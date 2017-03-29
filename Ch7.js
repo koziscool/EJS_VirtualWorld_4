843132d00b939590736ea33bbe3d7a5f742946f7
koziscool <kozjob@yahoo.com>
2017-03-28 23:22:10 -0500

    again

--- Ch7.js
+++ Ch7.js
@@ -124,6 +124,63 @@ World.prototype.toString = function() {
   return ret_str;
 };
 
+World.prototype.turn = function() {
+  var acted = [];
+  this.grid.forEach(function(critter, vector) {
+    if (critter.act && acted.indexOf(critter) == -1) {
+      acted.push(critter);
+      this.letAct(critter, vector);
+    }
+  }, this);
+};
+
+World.prototype.letAct = function(critter, vector) {
+  var action = critter.act(new View(this, vector));
+  if (action && action.type == "move") {
+    var dest = this.checkDestination(action, vector);
+    if (dest && this.grid.get(dest) == null) {
+      this.grid.set(vector, null);
+      this.grid.set(dest, critter);
+    }
+  }
+};
+
+World.prototype.checkDestination = function(action, vector) {
+  if (directionVectors.hasOwnProperty(action.direction)) {
+    var dest = vector.plus(directionVectors[action.direction]);
+    if (this.grid.contains(dest))
+      return dest;
+  }
+};
+
+var View = function(world, vector) {
+  this.world = world;
+  this.vector = vector;
+};
+
+View.prototype.look = function(dir) {
+  var target = this.vector.plus(directionVectors[dir]);
+  if (this.world.grid.contains(target))
+    return charFromElement(this.world.grid.get(target));
+  else
+    return "#";
+};
+
+View.prototype.findAll = function(ch) {
+  var found = [];
+  for (var dir in directionVectors)
+    if (this.look(dir) == ch)
+      found.push(dir);
+  return found;
+};
+
+View.prototype.find = function(ch) {
+  var found = this.findAll(ch);
+  if (found.length == 0) return null;
+  return randomElement(found);
+};
+
+
 var world = new World( plan, 
   {
     "#": Wall,
@@ -132,3 +189,8 @@ var world = new World( plan,
 );
 
 console.log( world.toString() );
+
+for (var i = 0; i < 5; i++) {
+  world.turn();
+  console.log(world.toString());
+}
